fd233027b6c17a5997908e8689241f1db227e028
FedericoBrunfman <federico.brunfman@patientpop.com>
2022-04-22 14:56:39 -0300

    add dist directory

---
+++ api/client/dist/precache-manifest.671bfff081c4bf6f62be17e3fcdf4fa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3bbb644126154531728",
+    "url": "/css/app.7ac416c2.css"
+  },
+  {
+    "revision": "08c3af18dbba72b4902f",
+    "url": "/css/chunk-vendors.18d88aff.css"
+  },
+  {
+    "revision": "c406d71dcc0a638db5dca01cfca4e234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3bbb644126154531728",
+    "url": "/js/app.249b2f9d.js"
+  },
+  {
+    "revision": "08c3af18dbba72b4902f",
+    "url": "/js/chunk-vendors.b2d5fd5d.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
